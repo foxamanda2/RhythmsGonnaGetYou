bbd398469f5a3c40efdf569ea5585db38316b21a
foxamanda2 <fox.amanda2@gmail.com>
2021-01-22 15:12:30 -0500

    View Menu Started

--- Program.cs
+++ Program.cs
@@ -10,10 +10,84 @@ namespace RhythmsGonnaGetYou
         {
             var context = new RhythmsGonnaGetYouContext();
 
-            foreach (var band in context.Band)
+            var albumCount = context.Albums.Count();
+            Console.WriteLine($"There are {albumCount} albums!");
+
+            foreach (var band in context.Bands)
             {
                 Console.WriteLine($"There is a movie named {band.Name}");
             }
+            // CODE STARTS HERE
+
+            Console.WriteLine("Welcome to Rhythms Gonna Get You Records Database:");
+
+            var userChoice = false;
+
+            while (userChoice == false)
+            {
+
+                Console.WriteLine("\n");
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine("View");
+                Console.WriteLine("Current Clients");
+                Console.WriteLine("Add");
+                Console.WriteLine("Contract Change");
+                Console.WriteLine("Quit");
+
+                var menuChoice = Console.ReadLine().ToLower().Trim();
+
+                switch (menuChoice)
+                {
+                    case "View":
+                        Console.WriteLine("View Options:");
+                        Console.WriteLine("All Bands");
+                        Console.WriteLine("Band Albums");
+                        Console.WriteLine("Albums by Band");
+
+                        var viewChoice = Console.ReadLine().ToLower().Trim();
+                        switch (viewChoice)
+                        {
+                            case "All Bands":
+
+
+                                break;
+                        }
+
+
+
+
+                        break;
+
+                }
+
+
+
+
+                //                 - View
+                //     - All bands
+                //     - Band Albums
+                //     - Album by Band
+                //   -Current Clients
+                //    - Signed
+                //    - Not Signed
+                //  - Add
+                //    - Add album for a band
+
+                //    -Add a song to album
+                //  - Contract change
+                //    - Let band go
+                //    - Resign a Band
+                //  - Quit
+
+
+
+            }
+
+
+
+
+
+
         }
     }
 }
